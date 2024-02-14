fc37a579f21c7225d5a6ebf796f707d7b205957e
ner <ner@pixmob.com>
2024-02-14 15:03:08 -0500

    a funkier _start loop
    will soon try a more complex example

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
-unsigned int n_loops = 0;
+unsigned int n_loops = 0, n_triggers = 0;
+unsigned int trigger = 1234;
 
 void _start (void)
 {
     while (1)
     {
         n_loops++;
+        if (n_loops == trigger) {
+            n_triggers++;
+        }
     }
 }
